a62d90419dbce3635036d2494818dc873e648c52
_omerock <83617943+AysKrimn@users.noreply.github.com>
2023-03-03 22:25:26 +0300

    Add files via upload

---
+++ script/handleToggleMenu.js
@@ -0,0 +1,9 @@
+
+
+function displayMenu() {
+    var menu = document.querySelector('.toggle');
+    var nav = document.querySelector('.navigation');
+
+    menu.classList.toggle('active');
+    nav.classList.toggle('active');
+}
